9b6dfd07de7cb2efa280c15eb42b61575623e8bd
xmasuhai <xmasuhai@163.com>
2020-10-12 00:07:38 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueButton />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueButton from './components/vuebutton/VueButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueButton,
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/vuebutton/VueButton.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="VueButton">
+  <button class="vue-button">HI</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss">
+:root{
+  --button-height: 32px;
+  --font-size: 14px;
+  --button-bg: white;
+  --button-active-bg: #eee;
+  --border-radius: 4px;
+  --color: #333;
+  --border-color: #999;
+  --border-color-hover: #666;
+  }
+
+.vue-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+   border-color: var(--border-color-hover);
+   }
+  &:active {
+    background-color: var(--button-active-bg);
+    }
+  &:focus {
+    outline: none;
+    }
+  }
+</style>
