cab20c078a87c281b6b1df9e786005a8cac71d2d
xmasuhai <xmasuhai@163.com>
2020-10-22 05:24:09 +0800

    props validator 验证 iconPosition 为 left 或 right 中的一个

--- src/components/vuebutton/VueButton.vue
+++ src/components/vuebutton/VueButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <!--
+<!--
   <div class="vue-demo-button">
     <button class="vue-button" v-if="icon-position === 'right'">
       <slot></slot>
@@ -15,21 +15,42 @@
     </button>
   </div>
   -->
-  <div class="vue-demo-button">
-    <button class="vue-button" :class="{[`icon-${iconPosition}`]: true}">
-      <svg v-if="icon" class="icon" aria-hidden="true">
-        <use :xlink:href="`#i-${icon}`"></use>
-      </svg>
-      <div class="content">
-        <slot></slot>
-      </div>
-    </button>
-  </div>
+<div class="vue-demo-button">
+  <button class="vue-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
+</div>
 </template>
 
 <script>
 export default {
-  props: ['icon', 'iconPosition'], // 'iconPosition': 'left' || 'right'
+  // props: ['icon', 'iconPosition'], // 'iconPosition': 'left' || 'right'
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(userValue) {
+        console.log(userValue)
+        /*
+        if (userValue !== 'left' && userValue !== 'right') {
+          return false
+        }else {
+          return true
+        }
+        */
+        // simplify if-else
+        // return value !== 'left' && value !== 'right' ? false : true;
+        // return !(userValue !== 'left' && userValue !== 'right');
+        return (userValue === 'left' || userValue === 'right');
+      },
+    }
+  },
   data() {
     return {}
   },
@@ -47,11 +68,13 @@ export default {
   --color: #333;
   --border-color: #999;
   --border-color-hover: #666;
-  }
+}
+
 .vue-demo-button {
   display: inline-block;
   margin-right: 10px;
-  }
+}
+
 .vue-button {
   display: inline-flex;
   justify-content: center;
@@ -64,36 +87,46 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+
   &:hover {
     border-color: var(--border-color-hover);
-    }
+  }
+
   &:active {
     background-color: var(--button-active-bg);
-    }
+  }
+
   &:focus {
     outline: none;
-    }
+  }
+
   &.icon-right {
-    > .icon {
+    >.icon {
       order: 2;
-      margin-left: .3em;margin-right: 0;
+      margin-left: .3em;
+      margin-right: 0;
       margin-top: .1em;
-      }
-    > .content {
-      order: 1;
-      }
     }
+
+    >.content {
+      order: 1;
+    }
+  }
+
   /* ali iconfont common css */
-  > .icon {
-    width: 1em; height: 1em;
-    margin-right: .3em;margin-left: 0;
+  >.icon {
+    width: 1em;
+    height: 1em;
+    margin-right: .3em;
+    margin-left: 0;
     margin-top: .1em;
     fill: currentColor;
     overflow: hidden;
     order: 1;
-    }
-  > .content {
-    order: 2;
-    }
   }
+
+  >.content {
+    order: 2;
+  }
+}
 </style>
