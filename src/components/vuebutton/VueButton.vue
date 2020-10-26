0aa40bc9051982f98f6330f99010a36d271a7f9d
xmasuhai <xmasuhai@163.com>
2020-10-26 21:43:24 +0800

    添加菊花状态

--- src/components/vuebutton/VueButton.vue
+++ src/components/vuebutton/VueButton.vue
@@ -1,37 +1,39 @@
 <template>
-<!--
-    <div class="vue-demo-button">
-      <button class="vue-button" v-if="icon-position === 'right'">
-        <slot></slot>
-        <svg v-if="icon" class="icon" aria-hidden="true">
-          <use :xlink:href="`#i-${icon}`"></use>
-        </svg>
-      </button>
-      <button class="vue-button" v-else>
-        <svg v-if="icon" class="icon" aria-hidden="true">
-          <use :xlink:href="`#i-${icon}`"></use>
-        </svg>
-        <slot></slot>
-      </button>
-    </div>
-    -->
-<div class="vue-demo-button">
-  <button class="vue-button" :class="{[`icon-${iconPosition}`]: true}">
-    <!-- 组件化 <Icon>
-      <svg v-if="icon" class="icon" aria-hidden="true">
-        <use :xlink:href="`#i-${icon}`"></use>
-      </svg>
+  <!--
+      <div class="vue-demo-button">
+        <button class="vue-button" v-if="icon-position === 'right'">
+          <slot></slot>
+          <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#i-${icon}`"></use>
+          </svg>
+        </button>
+        <button class="vue-button" v-else>
+          <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#i-${icon}`"></use>
+          </svg>
+          <slot></slot>
+        </button>
+      </div>
       -->
-    <VueIcon v-if="!!icon" :name="icon" class="icon" />
-    <div class="content">
-      <slot />
-    </div>
-  </button>
-</div>
+  <div class="vue-demo-button">
+    <button class="vue-button" :class="{[`icon-${iconPosition}`]: true}">
+      <!-- 组件化 <Icon>
+        <svg v-if="icon" class="icon" aria-hidden="true">
+          <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        -->
+      <VueIcon v-if="!!icon" :name="icon" class="icon"/>
+      <VueIcon name="loading" class="loading icon"/>
+      <div class="content">
+        <slot/>
+      </div>
+    </button>
+  </div>
 </template>
 
 <script>
 import VueIcon from '../icon/Icon.vue'
+
 export default {
   // props: ['icon', 'iconPosition'], // 'iconPosition': 'left' || 'right'
   props: {
@@ -68,6 +70,14 @@ export default {
 </script>
 
 <style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+    }
+  100% {
+    transform: rotate(360deg);
+    }
+  }
 :root {
   --button-height: 32px;
   --font-size: 14px;
@@ -77,13 +87,11 @@ export default {
   --color: #333;
   --border-color: #999;
   --border-color-hover: #666;
-}
-
+  }
 .vue-demo-button {
   display: inline-block;
   margin-right: 10px;
-}
-
+  }
 .vue-button {
   display: inline-flex;
   justify-content: center;
@@ -96,34 +104,28 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-
   &:hover {
     border-color: var(--border-color-hover);
-  }
-
+    }
   &:active {
     background-color: var(--button-active-bg);
-  }
-
+    }
   &:focus {
     outline: none;
-  }
-
+    }
   &.icon-right {
-    >.icon {
+    > .icon {
       order: 2;
       margin-left: .3em;
       margin-right: 0;
       margin-top: .1em;
-    }
-
-    >.content {
+      }
+    > .content {
       order: 1;
+      }
     }
-  }
-
   /* ali iconfont common css */
-  >.icon {
+  > .icon {
     width: 1em;
     height: 1em;
     margin-right: .3em;
@@ -132,10 +134,12 @@ export default {
     fill: currentColor;
     overflow: hidden;
     order: 1;
-  }
-
-  >.content {
+    }
+  > .content {
     order: 2;
+    }
+  .loading {
+    animation: spin 1s infinite linear;
+    }
   }
-}
 </style>
