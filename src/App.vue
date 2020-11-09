4cd0bf4387d6e23dccc4c0deecfc2a8546b0a680
xmasuhai <xmasuhai@163.com>
2020-11-09 23:28:19 +0800

    使用Karma做自动化测试

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ import VueButtonGroup from './components/ButtonGroup/ButtonGroup.vue'
 
 import VueButton2 from './components/vuebutton/VueButton'
 import VueIcon from './components/icon/Icon'
+
 // 单元测试
 import chai from 'chai'
 import spies from 'chai-spies'
@@ -39,114 +40,121 @@ Vue.component('v-button-group', VueButtonGroup)
 chai.use(spies)
 const expect = chai.expect
 
-// 测试 icon
-{
-  const Constructor = Vue.extend(VueButton2)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    },
-  })
-  // button.$mount('#test')
-  vm.$mount()
-  const useElement = vm.$el.querySelector('use')
-  // console.log(userElement)
-  /*
-  // Uncaught AssertionError
-  // expect(useElement.getAttribute('xlink:href')).to.equal('settings')
-  */
-  // 可以通过的测试用例
-  expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
-  const href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-settings')
-}
+try {
+// 测试按钮含有 icon
+  {
+    const Constructor = Vue.extend(VueButton2)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      },
+    })
+    // button.$mount('#test')
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    // console.log(userElement)
+    /*
+    // Uncaught AssertionError
+    // expect(useElement.getAttribute('xlink:href')).to.equal('settings')
+    */
+    // 可以通过的测试用例
+    expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+  }
 
-// 测试 isLoading
-{
-  const Constructor = Vue.extend(VueButton2)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      isLoading: true,
-    },
-  })
-  vm.$mount()
-  const useElement = vm.$el.querySelector('use')
-  const href = useElement.getAttribute('xlink:href')
-  // Uncaught AssertionError
-  // expect(href).to.eq('#i-settings')
-  expect(href).to.eq('#i-loading')
-}
+// 测试 isLoading属性 是否显示#i-loading的图标
+  {
+    const Constructor = Vue.extend(VueButton2)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        isLoading: true,
+      },
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    // Uncaught AssertionError
+    // expect(href).to.eq('#i-settings')
+    expect(href).to.eq('#i-loading')
+  }
 
 // 测试 iconPosition order 默认为 1 左边 ；2 右边
-{
-  const Constructor = Vue.extend(VueButton2)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    },
-  })
-  // button 必须被加载 渲染到页面中，CSS加载，才能识别 order，否则 expect(order).to.eq(‘’)为空
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  vm.$mount(div)
-  const svg = vm.$el.querySelector('svg')
-  // const order = window.getComputedStyle(svg).order
-  // ES6+ 解构赋值
-  const {order} = window.getComputedStyle(svg)
-  // 错误断言
-  // expect(order).to.eq(1)
-  // 正确断言
-  //CSS 所有属性值都是字符串
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
+  {
+    const Constructor = Vue.extend(VueButton2)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      },
+    })
+    // button 必须被加载 渲染到页面中，CSS加载，才能识别 order，否则 expect(order).to.eq(‘’)为空
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    vm.$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    // const order = window.getComputedStyle(svg).order
+    // ES6+ 解构赋值
+    const {order} = window.getComputedStyle(svg)
+    // 错误断言
+    // expect(order).to.eq(1)
+    // 正确断言
+    //CSS 所有属性值都是字符串
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
 
 // 测试 iconPosition order 为 2 右边
-{
-  const Constructor = Vue.extend(VueButton2)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPosition: 'right',
-    },
-  })
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  vm.$mount(div)
-  const svg = vm.$el.querySelector('svg')
-  const {order} = window.getComputedStyle(svg)
-  // 错误断言
-  // expect(order).to.eq("1")
-  // 正确断言
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
+  {
+    const Constructor = Vue.extend(VueButton2)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right',
+      },
+    })
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    vm.$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svg)
+    // 错误断言
+    // expect(order).to.eq("1")
+    // 正确断言
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
 
 // 测试 按钮触发 click // mock 使用 chai.spies 监听函数
-{
-  const Constructor = Vue.extend(VueButton2)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    },
+  {
+    const Constructor = Vue.extend(VueButton2)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      },
+    })
+    vm.$mount()
+    // console.log(chai.spy)
+    const spy = chai.spy(() => {
+      console.log('spy OK')
+    })
+    vm.$on('click', spy)
+    const button = vm.$el
+    button.click()
+    // 期望 函数被执行
+    // 正确断言
+    expect(spy).to.have.been.called()
+  }
+} catch (error) {
+  window.errors = [error]
+} finally {
+  // window.errors 存在 打印所有错误信息
+  window.errors && window.errors.forEach((error) => {
+    console.error(error.message)
   })
-  vm.$mount()
-  // console.log(chai.spy)
-  const spy = chai.spy(() => {
-    console.log('spy OK')
-  })
-  vm.$on('click', spy)
-  const button = vm.$el
-  button.click()
-  // 期望 函数被执行
-  // 错误断言
-  // 正确断言
-  expect(spy).to.have.been.called()
 }
-
 export default {
   name: 'App',
   data() {
--- src/components/vuebutton/VueButton.vue
+++ src/components/vuebutton/VueButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <!--
+<!--
       <div class="vue-demo-button">
         <button class="vue-button" v-if="icon-position === 'right'">
           <slot></slot>
@@ -15,25 +15,30 @@
         </button>
       </div>
       -->
-  <!--  <div class="vue-demo-button"> -->
-  <button class="vue-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
-    <!-- 组件化 <Icon>
+<!--  <div class="vue-demo-button"> -->
+<button class="vue-button" :class="{ [`icon-${iconPosition}`]: true }" @click="$emit('click')">
+  <!-- 组件化 <Icon>
       <svg v-if="icon" class="icon" aria-hidden="true">
         <use :xlink:href="`#i-${icon}`"></use>
       </svg>
       -->
-    <VueIcon v-if="!!icon && !isLoading" :name="icon" class="icon" @click="kClick"/>
-    <VueIcon v-if="isLoading" name="loading" class="loading icon"/>
-    <div class="content">
-      <slot/>
-    </div>
-  </button>
-  <!--  </div> -->
+  <VueIcon v-if="!!icon && !isLoading" :name="icon" class="icon" @click="kClick" />
+  <VueIcon v-if="isLoading" name="loading" class="loading icon" />
+  <div class="content">
+    <slot />
+  </div>
+</button>
+<!--  </div> -->
 </template>
 
 <script>
-import VueIcon from '../icon/Icon.vue'
-
+import VueIcon from "../icon/Icon.vue";
+/*
+// 全局注册 icon
+import Vue from "vue";
+import Icon from "../icon/Icon.vue";
+Vue.component("v-icon", Icon);
+ */
 export default {
   // props: ['icon', 'iconPosition'], // 'iconPosition': 'left' || 'right'
   props: {
@@ -46,9 +51,9 @@ export default {
     },
     iconPosition: {
       type: String,
-      default: 'left',
+      default: "left",
       validator(userValue) {
-        console.log(userValue)
+        console.log(userValue);
         /*
         if (userValue !== 'left' && userValue !== 'right') {
           return false
@@ -59,33 +64,35 @@ export default {
         // simplify if-else
         // return value !== 'left' && value !== 'right' ? false : true;
         // return !(userValue !== 'left' && userValue !== 'right');
-        return (userValue === 'left' || userValue === 'right')
+        return userValue === "left" || userValue === "right";
       },
     },
   },
   data() {
-    return {}
+    return {};
   },
   methods: {
     kClick() {
-      this.$emit('click')
+      this.$emit("click");
     },
   },
   components: {
-    VueIcon
+    VueIcon,
   },
-}
+};
 </script>
 
 <style lang="scss">
 @keyframes spin {
   0% {
     transform: rotate(0deg);
-    }
+  }
+
   100% {
     transform: rotate(360deg);
-    }
   }
+}
+
 :root {
   --button-height: 32px;
   --font-size: 14px;
@@ -96,7 +103,8 @@ export default {
   --border-color: #999;
   //--border-color-hover: red;
   --border-color-hover: #666;
-  }
+}
+
 /*
 .vue-demo-button {
   display: inline-flex;
@@ -112,48 +120,56 @@ export default {
   vertical-align: middle;
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 .73em;
+  padding: 0 0.73em;
   font: inherit;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+
   &:hover {
     border-color: var(--border-color-hover);
-    }
+  }
+
   &:active {
     background-color: var(--button-active-bg);
-    }
+  }
+
   &:focus {
     outline: none;
-    }
+  }
+
   &.icon-right {
-    > .icon {
+    >.icon {
       order: 2;
-      margin-left: .3em;
+      margin-left: 0.3em;
       margin-right: 0;
-      margin-top: .1em;
-      }
-    > .content {
-      order: 1;
-      }
+      margin-top: 0.1em;
     }
+
+    >.content {
+      order: 1;
+    }
+  }
+
   /* ali iconfont common css */
-  > .icon {
+  >.icon {
     width: 1em;
     height: 1em;
-    margin-right: .3em;
+    margin-right: 0.3em;
     margin-left: 0;
-    margin-top: .1em;
+    margin-top: 0.1em;
     fill: currentColor;
     overflow: hidden;
     order: 1;
     //fill: red;
-    }
-  > .content {
+  }
+
+  >.content {
     order: 2;
-    }
+  }
+
   .loading {
     animation: spin 1s infinite linear;
-    }
   }
+}
 </style>
