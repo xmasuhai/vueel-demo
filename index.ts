e5bc0d4d11cb7721a7fe22b9d3ba37bd6e5ab1d2
xmasuhai <xmasuhai@163.com>
2021-07-15 06:50:44 +0800

    创建npm包入口文件index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import Button from './src/components/vuebutton/VueButton.vue'
+import ButtonGroup from './src/components/ButtonGroup/ButtonGroup.vue'
+import Icon from './src/components/icon/VueIcon.vue'
+
+export {Button, ButtonGroup, Icon}
