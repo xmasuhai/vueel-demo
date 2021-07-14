2058f5a42a38f884e6e1f8d69744c8764113a7b0
xmasuhai <xmasuhai@163.com>
2021-07-14 10:28:15 +0800

    修改karma配置 只使用ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = (config) => {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
