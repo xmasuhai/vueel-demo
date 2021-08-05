423aecf121b3db9c08b0e6e692804fce62fe2287
xmasuhai <xmasuhai@163.com>
2021-08-05 15:56:20 +0800

    :white_check_mark: 更新 测试用例

--- test/row.test.ts
+++ test/row.test.ts
@@ -9,7 +9,6 @@ const expect = chai.expect;
 
 import VueRow from '../src/components/grid-system/VueRow.vue';
 import VueCol from '../src/components/grid-system/VueCol.vue';
-import {mount} from '@vue/test-utils';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
@@ -73,22 +72,6 @@ describe('VueRow', () => {
 
     });
 
-  });
-
-  describe('VueRow.vue', () => {
-    let wrapper: Vue;
-
-    beforeEach(() => {
-      wrapper = mount(VueRow, {
-        propsData: {
-          gutter: 20
-        }
-      });
-    });
-
-    it('should contain gutter', function () {
-      expect(wrapper.find('.row').isVisible()).to.eq(true);
-    });
   });
 
   /*
