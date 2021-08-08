8e48a1962ca2edb6e8e9ae34a59b5f527f4f00b8
xmasuhai <xmasuhai@163.com>
2021-08-08 12:47:57 +0800

    :construction: 组件占坑

--- src/components/button/VueButtonGroup.vue
+++ src/components/button/VueButtonGroup.vue
@@ -15,8 +15,8 @@ export default class VueIcon extends Vue {
     for (const node of Array.from(this.$el.children)) {
       const name = node.nodeName.toLowerCase();
       if (name !== 'button') {
-        console.warn(`vue-button-group 的子元素应该全是 VueButton，
-        但你写的是${name}`);
+        // console.warn(`vue-button-group 的子元素应该全是 VueButton，
+        // 但你写的是${name}`);
       }
     }
   }
