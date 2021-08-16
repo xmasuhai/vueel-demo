de43f46935a1e058c1e63af48cd552dd4e38010e
xmasuhai <xmasuhai@163.com>
2021-08-16 12:12:43 +0800

    :art: 重构 toastPlugin.ts

--- src/components/toast/toastPlugin.ts
+++ src/components/toast/toastPlugin.ts
@@ -1,14 +1,15 @@
 import Toast from './VueToast.vue';
+import Vue from 'vue';
 import {VueToastOptions} from '@/types/VueToast';
 
 export default {
-  install(Vue: any, options: {} = {}) {
-    Vue.prototype.$toast = (message: string | HTMLElement, toastOptions: VueToastOptions | undefined) => {
+  install(/*Vue: Vue, options: {} = {}*/) {
+    Vue.prototype.$toast = (message: string, toastOptions: VueToastOptions | undefined) => {
       const Constructor = Vue.extend(Toast);
       const toast = new Constructor({
         propsData: toastOptions?.propsData
       });
-      toast.$slots.default = [message];
+      toast.$slots.default = [message as any];
       toast.$mount();
       document.body.appendChild(toast.$el);
     };
