fce48a027497a5f8b95c4f2e207e863ff43e89c5
xmasuhai <xmasuhai@163.com>
2021-08-27 07:12:34 +0800

    :construction: 处理判断点击事件目标对象 的逻辑

--- src/components/popover/VuePopover.vue
+++ src/components/popover/VuePopover.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="popover" @click.stop="togglePop">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="togglePop">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span class="trigger" ref="trigger">
-      <slot></slot>
+    <span class="triggerWrapper" ref="triggerWrapper">
+      <slot>botton</slot>
     </span>
   </div>
 </template>
@@ -17,18 +20,70 @@ export default class VuePopover extends Vue {
   name = 'VuePopover';
   visible = false;
 
-  togglePop() {
-    this.visible = !this.visible;
+  togglePop(event: Event) {
 
-    if (this.visible) {
-      this.$nextTick(() => {
-        document.body.appendChild(this.$refs.contentWrapper as Node);
-        const {top, left}
-          = (this.$refs.trigger as Element).getBoundingClientRect();
-        (this.$refs.contentWrapper as any).style.left = `${left + window.scrollX}px`;
-        (this.$refs.contentWrapper as any).style.top = `${top + window.scrollY}px`;
-      });
+    if ((this.$refs.triggerWrapper as HTMLElement)
+      .contains(event.target as Node)) {
+      this.visible = !this.visible;
+      if (this.visible) {
+
+        console.log('---------------');
+        this.$nextTick(() => {
+          // 获取弹出消息框节点的引用
+          document.body.appendChild(this.$refs.contentWrapper as Node);
+          const {top, left}
+            = (this.$refs.triggerWrapper as Element)
+            .getBoundingClientRect();
+          (this.$refs.contentWrapper as any).style.left = `${left + window.scrollX}px`;
+          (this.$refs.contentWrapper as any).style.top = `${top + window.scrollY}px`;
+
+          const eventHandler = () => {
+            this.visible = false;
+            console.log('触发事件回调', this.visible);
+            document.removeEventListener('click', eventHandler);
+          };
+
+          console.log('添加监听事件');
+          document.addEventListener('click', eventHandler);
+
+        });
+
+      }
+
+    } else {
+      console.log('x上面');
     }
+
+    /*
+
+        if (this.visible) {
+
+          console.log('---------------');
+          this.$nextTick(() => {
+            // 获取弹出消息框节点的引用
+            document.body.appendChild(this.$refs.contentWrapper as Node);
+            const {top, left}
+              = (this.$refs.trigger as Element).getBoundingClientRect();
+            (this.$refs.contentWrapper as any).style.left = `${left + window.scrollX}px`;
+            (this.$refs.contentWrapper as any).style.top = `${top + window.scrollY}px`;
+
+            const eventHandler = () => {
+              console.log('触发事件回调');
+              this.visible = false;
+              document.removeEventListener('click', eventHandler);
+            };
+
+            console.log('添加监听事件');
+            document.addEventListener('click', eventHandler);
+
+          });
+
+        } else {
+          console.log('关闭');
+        }
+    */
+
+
   }
 
 }
