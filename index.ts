a75b6e96a9b3dab509f57d53ee368329fef7496e
xmasuhai <xmasuhai@163.com>
2021-09-17 15:15:57 +0800

    :bookmark: 更新版本version 0.0.4

--- index.ts
+++ index.ts
@@ -1,5 +1,47 @@
-import VueButton from './src/components/button/VueButton.vue'
-import VueButtonGroup from './src/components/button/VueButtonGroup.vue'
-import VueIcon from './src/components/icon/VueIcon.vue'
+import VueButton from './src/components/button/VueButton.vue';
+import VueButtonGroup from './src/components/button/VueButtonGroup.vue';
+import VueIcon from './src/components/icon/VueIcon.vue';
+import VueInput from './src/components/input/VueInput.vue';
+import VueRow from './src/components/grid/VueRow.vue';
+import VueCol from './src/components/grid/VueCol.vue';
+import VueMain from './src/components/layout/VueMain.vue';
+import VueContainer from './src/components/layout/VueContainer.vue';
+import VueAside from './src/components/layout/VueAside.vue';
+import VueFooter from './src/components/layout/VueFooter.vue';
+import VueHeader from './src/components/layout/VueHeader.vue';
+import VueToast from './src/components/toast/VueToast.vue';
+import toastPlugin from './src/components/toast/toastPlugin';
+import VueTab from './src/components/tabs/VueTab.vue';
+import VueTabNav from './src/components/tabs/VueTabNav.vue';
+import VueTabItem from './src/components/tabs/VueTabItem.vue';
+import VueTabContent from './src/components/tabs/VueTabContent.vue';
+import VueTabPane from './src/components/tabs/VueTabPane.vue';
+import VuePopover from './src/components/popover/VuePopover.vue';
+import VueCollapse from './src/components/collapse/VueCollapse.vue';
+import VueCodePresentation from './src/components/code-presentation/VueCodePresentation.vue';
+import VueDetail from './src/components/code-presentation/VueDetail.vue';
 
-export {VueButton, VueButtonGroup, VueIcon}
+export {
+  VueButton,
+  VueButtonGroup,
+  VueIcon,
+  VueInput,
+  VueRow,
+  VueCol,
+  VueMain,
+  VueContainer,
+  VueAside,
+  VueFooter,
+  VueHeader,
+  VueToast,
+  toastPlugin,
+  VueTab,
+  VueTabNav,
+  VueTabItem,
+  VueTabContent,
+  VueTabPane,
+  VuePopover,
+  VueCollapse,
+  VueCodePresentation,
+  VueDetail
+};
