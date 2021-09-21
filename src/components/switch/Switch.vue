dc7c7bbf5cb7b3e2c5ad850098a2b3c083685904
xmasuhai <xmasuhai@163.com>
2021-09-22 06:26:02 +0800

    :construction: 使用 @vue/composition-api 创建 Switch.vue组件

---
+++ src/components/switch/Switch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Switch
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, getCurrentInstance} from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Switch',
+  setup(props, ctx) {
+    console.log(props);
+    console.log(getCurrentInstance());
+    console.log(ctx);
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
