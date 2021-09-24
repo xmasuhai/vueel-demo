819d104a6e385377bf43dd91ca2b708dcdb95245
xmasuhai <xmasuhai@163.com>
2021-09-24 16:46:10 +0800

    :construction_worker: :green_heart: :rocket: typescript新增tsLoaderOptions配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -116,7 +116,49 @@ module.exports = (/*options, context*/) => ({
       'vuepress-plugin-typescript',
       {
         // ts-loader 的所有配置项
-        tsLoaderOptions: {}
+        tsLoaderOptions: {
+          "compilerOptions": {
+            "target": "es5",
+            "module": "esnext",
+            "strict": true,
+            "jsx": "preserve",
+            "importHelpers": true,
+            "moduleResolution": "node",
+            "experimentalDecorators": true,
+            "skipLibCheck": true,
+            "esModuleInterop": true,
+            "allowSyntheticDefaultImports": true,
+            "sourceMap": true,
+            "baseUrl": ".",
+            "types": [
+              "webpack-env",
+              "jest",
+              "vuepress-types"
+            ],
+            "paths": {
+              "@/*": [
+                "src/*"
+              ]
+            },
+            "lib": [
+              "esnext",
+              "dom",
+              "dom.iterable",
+              "scripthost"
+            ]
+          },
+          "include": [
+            "src/**/*.ts",
+            "src/**/*.tsx",
+            "src/**/*.vue",
+            "tests/**/*.ts",
+            "tests/**/*.tsx",
+            "docs/.vuepress/enhanceApp.js"
+          ],
+          "exclude": [
+            "node_modules"
+          ]
+        }
       }
     ]
   ],
