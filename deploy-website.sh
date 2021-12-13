fd6da396aa38320b493130f102f23fa71cc48aba
xmasuhai <xmasuhai@163.com>
2021-12-14 07:38:22 +0800

    :sparkles: 添加 book 展示组件

--- deploy-website.sh
+++ deploy-website.sh
@@ -3,16 +3,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+rm -rf dist &&
 # 生成静态文件
-yarn docs:build
+yarn docs:build &&
 
 # 进入生成的文件夹
-cd ./dist
+cd ./dist &&
 
-git init
-git add -A
-git commit -m 'deploy'
+git init &&
+git add -A &&
+git commit -m 'deploy' &&
 
-git push -f git@github.com:xmasuhai/vueel-demo-website.git master:gh-pages
-
+git push -f git@github.com:xmasuhai/vueel-demo-website.git master:gh-pages &&
+git remote add origin https://gitee.com/xmasuhai/vue-demo-1.git &&
+git push -u origin master
 cd -
--- src/components/BooksInfo.vue
+++ src/components/BooksInfo.vue
@@ -5,18 +5,18 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "@vue/composition-api";
+import {defineComponent} from '@vue/composition-api';
 
 export default defineComponent({
-  name: "BooksInfo",
+  name: 'BooksInfo',
   props: {},
   setup(props, ctx) {
-    const a = [props, ctx]
-    a.push(1)
+    const a = [props, ctx];
+    a.push(1);
   }
 });
 </script>
 
 <style lang="scss" scoped>
-
+@import './book-demo/copystrap.scss';
 </style>
