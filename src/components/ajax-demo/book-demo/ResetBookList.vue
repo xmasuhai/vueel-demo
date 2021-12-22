7aead1ad38b4df17c688ba0d59ff8ee6a9fccd6b
xmasuhai <xmasuhai@163.com>
2021-12-22 16:54:14 +0800

    :sparkles: 重构 一键还原 初始图书列表 组件 ResetBookList.vue 再次检查

--- src/components/ajax-demo/book-demo/ResetBookList.vue
+++ src/components/ajax-demo/book-demo/ResetBookList.vue
@@ -23,31 +23,47 @@ export default defineComponent({
   setup(/*props, ctx*/) {
     const eventbus = inject<Vue>('eventbus');
     const toast = inject<Function>('toast');
+
     const resetBookList = () => {
       // 获取图书列表 rows
       let rows = getBookInfo();
 
-      rows.value.forEach((item) => {
-        const id = item.id;
+      // 按条件 重置列表
+      if(rows.value.length > 3) {
+        rows.value.forEach((item) => {
+          const id = item.id;
 
-        // 根据图书id 依次删除
-        delBook({id: `${id}`})
-          .then(res => {
-            if (res.status === 200) {
-              const {data} = res.data;
-              rows = data;
-            }
-          })
-          .catch(err => {
-            return toast && toast(`删除图书失败！${err.message}`);
-          });
+          // 根据图书id 依次删除
+          (Number(id) > 3) && delBook({id: `${id}`})
+            .then(res => {
+              if (res.status === 200) {
+                const {data} = res.data;
+                rows = data;
+              }
+            })
+            .catch(err => {
+              return toast && toast(`删除图书失败！${err.message}`);
+            });
 
-      });
+        });
+        watchRenderBookList(getBookInfo(), eventbus);
 
-      watchRenderBookList(getBookInfo(), eventbus);
-      return toast && toast('还原初始数据成功！');
+/*
 
+        // 再次检查
+        rows = getBookInfo();
+        (rows.value.length > 3)
+          ? resetBookList()
+          : toast && toast('无需还原初始数据！');
+*/
+
+
+        return toast && toast('还原初始数据成功！');
+      }
+
+      return toast && toast('保留初始数据！');
     };
+
     return {
       resetBookList
     };
