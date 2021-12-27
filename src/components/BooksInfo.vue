0f9b9e423c759de81f8500c81866094bedfbfe8f
xmasuhai <xmasuhai@163.com>
2021-12-28 07:37:11 +0800

    ok

--- src/components/BooksInfo.vue
+++ src/components/BooksInfo.vue
@@ -6,6 +6,9 @@
     <ResetBookList></ResetBookList>
     <!-- 展示图书的表格 -->
     <ShowBookInfo></ShowBookInfo>
+    <hr/>
+    <!--FormData-->
+    <FormData></FormData>
   </div>
 </template>
 
@@ -13,12 +16,14 @@
 import AddBookInfo from '@/components/ajax-demo/book-demo/AddBookInfo.vue';
 // import ShowBookInfo from '@/components/ajax-demo/book-demo/ShowBookInfo.vue';
 import ResetBookList from '@/components/ajax-demo/book-demo/ResetBookList.vue';
+import FormData from '@/components/ajax-demo/formdata/FormData.vue';
 import {defineAsyncComponent, defineComponent, provide, reactive} from '@vue/composition-api';
 import Vue from 'vue';
 
 export default defineComponent({
   name: 'BooksInfo',
   components: {
+    FormData,
     AddBookInfo,
     ShowBookInfo: defineAsyncComponent(() => import('./ajax-demo/book-demo/ShowBookInfo.vue')),
     ResetBookList
---
+++ src/components/ajax-demo/formdata/FormData.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form ref="form1">
+      <input type="text" name="uname" autocomplete="off"/>
+      <input type="password" name="upwd" autocomplete="off"/>
+      <button type="submit">提交</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'FormData',
+  props: {},
+  setup(props, ctx) {
+    // console.log("-> ctx", ctx);
+    /*
+    const form = ctx.slots('#form1');
+    // 监听表单元素的 submit 事件
+    form.addEventListener('submit', (e) => {
+      e.preventDefault(); // 使用 Ajax 的形式提交表单避险阻止默认提交行为
+      // 根据 form 表单创建 FormData 对象，会自动将表单数据填充到 FormData 对象中，无需手动 调用 .append 方法
+      const fd = new FormData(form);
+      // 无需设置 `xhr.setHeader` 的数据类型
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://www.liulongbin.top:3006/api/formdata');
+      xhr.onreadystatechange = () => {};
+      xhr.send(fd);
+
+    });
+    */
+
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
