00ab5995088d949cda48f8cbfd7ef21003bdcfae
xmasuhai <xmasuhai@163.com>
2021-12-29 07:37:09 +0800

    :sparkles: 使用`XMLHttpRequest Level2`上传文件 封装 Upload.vue 示例组件 实现 验证是否选择了文件

--- src/components/ajax-demo/formdata/Upload.vue
+++ src/components/ajax-demo/formdata/Upload.vue
@@ -13,23 +13,21 @@
 import VueButton from '@/components/button/VueButton.vue';
 import VueInput from '@/components/input/VueInput.vue';
 import {defineComponent} from '@vue/composition-api';
+import Vue from 'vue';
 
 export default defineComponent({
   name: 'Upload',
   components: {VueInput, VueButton},
   props: {},
-  setup(/*props, ctx*/) {
+  setup(props, ctx) {
     const uploadFile = () => {
-      console.log('1');
+      const files = (ctx.refs.file1 as Vue).$el.querySelector('input')?.files;
+      if (files && files.length <= 0) {return alert('请选择要上传的文件');}
     };
 
     return {
       uploadFile
-    }
+    };
   }
 });
 </script>
-
-<style lang="scss" scoped>
-
-</style>
