fc31b558108919fe22d153c1ef875d7aac5cd5f3
xmasuhai <xmasuhai@163.com>
2021-12-29 07:17:24 +0800

    :sparkles: 使用`XMLHttpRequest Level2`上传文件 封装 Upload.vue 示例组件

--- src/components/BooksInfo.vue
+++ src/components/BooksInfo.vue
@@ -9,6 +9,8 @@
     <hr/>
     <!--FormData-->
     <FormData></FormData>
+    <!-- 上传文件 -->
+    <Upload></Upload>
   </div>
 </template>
 
@@ -17,12 +19,14 @@ import AddBookInfo from '@/components/ajax-demo/book-demo/AddBookInfo.vue';
 // import ShowBookInfo from '@/components/ajax-demo/book-demo/ShowBookInfo.vue';
 import ResetBookList from '@/components/ajax-demo/book-demo/ResetBookList.vue';
 import FormData from '@/components/ajax-demo/formdata/FormData.vue';
+import Upload from '@/components/ajax-demo/formdata/Upload.vue';
 import {defineAsyncComponent, defineComponent, provide, reactive} from '@vue/composition-api';
 import Vue from 'vue';
 
 export default defineComponent({
   name: 'BooksInfo',
   components: {
+    Upload,
     FormData,
     AddBookInfo,
     ShowBookInfo: defineAsyncComponent(() => import('./ajax-demo/book-demo/ShowBookInfo.vue')),
---
+++ src/components/ajax-demo/formdata/Upload.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <VueInput type="file" ref="file1"/>
+    <VueButton ref="btnUpload"
+               @click="uploadFile">上传图片
+    </VueButton>
+    <br/>
+    <img src="" alt="" ref="img" width="800">
+  </div>
+</template>
+
+<script lang="ts">
+import VueButton from '@/components/button/VueButton.vue';
+import VueInput from '@/components/input/VueInput.vue';
+import {defineComponent} from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Upload',
+  components: {VueInput, VueButton},
+  props: {},
+  setup(/*props, ctx*/) {
+    const uploadFile = () => {
+      console.log('1');
+    };
+
+    return {
+      uploadFile
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
