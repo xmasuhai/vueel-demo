b7e8d0768900a036b5c9a1e5e5d76bfbca1d3ebb
xmasuhai <xmasuhai@163.com>
2021-12-31 11:09:09 +0800

    ♻️ 重构代码

--- src/components/ajax-demo/book-demo/ResetBookList.vue
+++ src/components/ajax-demo/book-demo/ResetBookList.vue
@@ -10,7 +10,6 @@
 import {delBook} from '@/components/ajax-demo/http-request/bookInfoOperations';
 import {getBookInfo} from '@/components/ajax-demo/util/getBookInfo';
 import {watchRenderBookList} from '@/components/ajax-demo/util/watchRenderBookList';
-// import {getBookList, delBook} from '@/components/ajax-demo/http-request/bookInfoOperations';
 import VueButton from '@/components/button/VueButton.vue';
 import {defineComponent, inject, Ref} from '@vue/composition-api';
 import Vue from 'vue';
@@ -25,43 +24,40 @@ export default defineComponent({
     const toast = inject<Function>('toast');
     let rows: Ref<{ [p: string]: string }[]>;
 
-    /*
-    const getaBook = () => {
-      getBook(1, '西游记');
-    }
-    */
-
     const resetBookList = (): void => {
       // 获取图书列表 rows
       rows = getBookInfo();
-      // 按条件 重置列表
-      if (rows.value.length === 3) { // 递归终止条件
+      /* 按条件 重置列表 */
+      // 递归终止条件 不清除默认数据
+      if (rows.value.length === 3) {
         return toast && toast('保留初始数据！');
-      } else {
-        rows.value.forEach((item) => {
-          const id = item.id;
-          // 根据图书id 依次删除
-          (Number(id) > 3) && delBook({id: `${id}`})
-            .then(res => {
-              if (res.status === 200) {
-                const {data} = res.data;
-                rows = data;
-              }
-            })
-            .catch(err => {
-              return toast && toast(`删除图书失败！${err.message}`);
-            });
-
-        });
-        // 监听，渲染图书列表
-        watchRenderBookList(getBookInfo(), eventbus);
-        return toast && toast('重置初始数据！') && resetBookList(); // 递归判断
       }
+
+      // 从第4项开始 遍历每一项 执行清除 delbook()
+      rows.value.forEach((item) => {
+        const id = item.id;
+        // 根据图书id 依次删除
+        (Number(id) > 3) && delBook({id: `${id}`})
+          .then(res => {
+            if (res.status === 200) {
+              const {data} = res.data;
+              rows = data;
+            }
+          })
+          .catch(err => {
+            return toast && toast(`删除图书失败！${err.message}`);
+          });
+
+      });
+
+      // 监听getBookInfo()依赖，更新渲染图书列表
+      watchRenderBookList(getBookInfo(), eventbus);
+      return toast && toast('重置初始数据！') && resetBookList(); // 递归判断
+
     };
 
     return {
-      resetBookList,
-      /*getaBook*/
+      resetBookList
     };
   }
 });
