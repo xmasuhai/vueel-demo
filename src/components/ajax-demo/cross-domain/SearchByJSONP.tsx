2e07629c5f7b95b63bc6d40671f5e4c353107c69
xmasuhai <xmasuhai@163.com>
2022-01-07 14:12:06 +0800

    ✨  使用 axios-jsonp 请求JSONP 数据 修改 SearchByJSONP.tsx 组件 完成发送JSONP请求逻辑 模拟淘宝搜索 展示关键字案例

--- src/components/ajax-demo/cross-domain/SearchByJSONP.tsx
+++ src/components/ajax-demo/cross-domain/SearchByJSONP.tsx
@@ -1,4 +1,4 @@
-import {defineComponent, Ref, ref,} from '@vue/composition-api';
+import {defineComponent, Ref, ref} from '@vue/composition-api';
 import style from '@/components/ajax-demo/cross-domain/SearchByJSONP.module.scss';
 import logo from '@/components/ajax-demo/cross-domain/taobao_logo.png';
 import axios from 'axios';
@@ -12,52 +12,66 @@ export default defineComponent({
   components: {},
   setup(/*props, ctx*/) {
     // 定义延时器的Id
-    const timer: Ref<ReturnType<typeof setTimeout> | undefined> = ref();
+    let timer: number;
     // 定义缓存对象
-    const cacheObj = ref({});
+    const cacheObj: Record<string, Array<Array<string>>> = {};
+    // 获取用户输入的字符 并去掉两边的空格
+    const inputValue = ref('');
 
-    // 接受数据，渲染UI结构
-    const renderSuggestList = (result: Array<string>) => {
-      /*
-        result.length <= 0
-          ?
-          : ;
-      */
-      return result
-    };
+    const responseData: Ref<Array<Array<string>>> = ref([]);
 
-    // 发送请求 搜索关键字
+    // 发送请求JSONP 搜索关键字
     const getSuggestList = (keyword: string) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
       // @ts-ignore
       axios({
         url: `https://suggest.taobao.com/sug?q=${keyword}`,
         adapter: jsonpAdapter,
-        callbackParamName: 'callback' // optional, 'callback' by default
+        // callbackParamName: 'callback' // optional, 'callback' by default
       })
         .then((res) => {
-          return res
+          const {data} = res;
+          const {result} = data;
+          result.length === 0
+            ? responseData.value = [['无搜索宝贝']]
+            : responseData.value = result;
+          // 将获取到用户输入的内容，当做键
+          // 将需要将数据作为值，进行缓存
+          cacheObj[inputValue.value] = result;
         });
     };
 
     // 定义防抖的函数
     const debounceSearch = (kw: string) => {
-      timer.value = setTimeout(function () {
+      timer = window.setTimeout(function () {
         getSuggestList(kw);
-      }, 500);
+      }, 800);
     };
 
-    // 为输入框绑定 keyup 事件
+    // 为输入框绑定 keyup 事件回调函数
+    const sendQuery = () => {
+      // 清空 timer
+      clearTimeout(timer);
 
+      // 无输入字符时，无需发起请求
+      if (inputValue.value.length === 0) {return;}
 
-    const suggestList = '';
+      // 判断缓存中对应的关键字如果有数据，更新数据 responseData
+      if (cacheObj[inputValue.value]) {
+        responseData.value = cacheObj[inputValue.value];
+      }
+
+      // 获取搜索建议列表 加防抖
+      // getSuggestList(keywords)
+      debounceSearch(inputValue.value);
+    };
 
     return {
-      suggestList,
       debounceSearch,
-      renderSuggestList,
+      sendQuery,
+      inputValue,
       cacheObj,
-      timer
+      responseData
     };
   },
   render() {
@@ -70,9 +84,10 @@ export default defineComponent({
              class={style.logo}/>
 
         <div class={style.box}>
+
           {/* tab 栏 */}
           <div class={style.tabs}>
-            <div class={style['tabs-active']}>宝贝</div>
+            <div class={style['tab-active']}>宝贝</div>
             <div>店铺</div>
           </div>
 
@@ -81,16 +96,32 @@ export default defineComponent({
             <input id="ipt"
                    type="text"
                    class={style.ipt}
+                   v-model={this.inputValue}
+                   onKeyup={this.sendQuery}
                    placeholder="请输入要搜索的内容"/>
             <button class={style.btnSearch}>搜索</button>
           </div>
 
           {/* 搜索建议列表 */}
-          <div id={this.suggestList}>
-            {}
-            <div class={style['suggest-item']}>123</div>
-          </div>
+          {/* 条件渲染 */}
+          {this.inputValue.length === 0
+            ? null
+            : (
+              <div class={style['suggest-list']}>
+                {/* 循环渲染 */}
+                {this.responseData.map((item, index) => (
+                  <div class={style['suggest-item']}
+                       key={index}>
+                    {item[0]}
+                  </div>
+                ))}
+              </div>
+            )
+
+          }
+
         </div>
+
       </div>
     );
   }
